9dbe16a2c90819324b978721808d440c90dbe3fb
Yuliya K <yuli-kretova@yndex.ru>
2022-11-12 16:27:27 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import mainLogo from "./mainLogo1.png";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src={mainLogo} alt='logo'></img>
+    </header>
+  );
+};
+
+export default Header;
