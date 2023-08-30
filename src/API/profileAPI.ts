9e3ec11f30812142bba8e876433562534dd0d3cb
Yuliya K <yuli-kretova@yndex.ru>
2023-08-30 21:10:56 +0300

    fixed photos type in profileAPI

--- src/API/profileAPI.ts
+++ src/API/profileAPI.ts
@@ -10,34 +10,35 @@ type APIResultType = {
 }
 
 type SavePhotoDataType = {
-  photos: PhotosType
+  data: {
+    photos: PhotosType
+  }
 }
 
 
-
 export const profileAPI = {
-  async getProfile(userId:number | null) {
+  async getProfile(userId: number | null) {
     const response = await axiosInstance
       .get<ProfileType>(`profile/${userId}`)
-      return response.data
-  },
-  async getStatus(userId:number | null) {
-    const response = await axiosInstance
-    .get<string>(`profile/status/${userId}`)
     return response.data
   },
-  async updateStatus(status:string) {
+  async getStatus(userId: number | null) {
     const response = await axiosInstance
-    .put<APIResultType>(`profile/status`, {status})
+      .get<string>(`profile/status/${userId}`)
+    return response.data
+  },
+  async updateStatus(status: string) {
+    const response = await axiosInstance
+      .put<APIResultType>(`profile/status`, { status })
     return response.data
   },
   async savePhoto(photoFile: File) {
     const formData = new FormData()
     formData.append("image", photoFile)
     const response = await axiosInstance.put<SavePhotoDataType>('profile/photo', formData)
-    return response.data
+    return response.data.data
   },
-  saveProfile(profile:ProfileType) {
+  saveProfile(profile: ProfileType) {
     return axiosInstance.put<APIResultType>('profile', profile)
 
   }
