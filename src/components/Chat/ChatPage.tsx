f143a2caa9e5a278b2f83a5d60ea5ae039796275
Yuliya K <yuli-kretova@yndex.ru>
2023-08-30 20:19:18 +0300

    made chat availability only when authorized

--- src/components/Chat/ChatPage.tsx
+++ src/components/Chat/ChatPage.tsx
@@ -1,14 +1,20 @@
 import React, { useCallback, useState } from 'react'
 import { CustomContentButton } from '../../UI/CustomContentButton/CustomContentButton';
 import { Chat } from './Chat';
+import { useSelector } from 'react-redux';
+import { AppStateType } from '../../redux/redux-store';
 
 export const ChatPage: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false)
   const openChat = () => setIsOpen(true)
   const closeChat = useCallback(() => setIsOpen(false), [])
+  const isAuth = useSelector((state: AppStateType) => state.auth.isAuth)
 
-  if (isOpen === false) {
+  if (isOpen === false && isAuth) {
     return <CustomContentButton onClick={openChat}>Open chat</CustomContentButton>
+  } else if (isAuth === false) {
+    return null
   }
   return <Chat closeChat={closeChat} />
 }
+
